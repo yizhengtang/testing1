833741df03296ce3f0d97e55eebf4c193544d811
G00425075@atu.ie <g00425075@atu.ie>
2024-12-13 13:32:43 +0000

    microservice 1 added

---
+++ micro1/src/test/java/ie/atu/micro1/Micro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.micro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
