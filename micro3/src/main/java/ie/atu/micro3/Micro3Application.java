3b5df46d66c8426d12fafae7561768f116c778a3
G00425075@atu.ie <g00425075@atu.ie>
2024-12-13 13:35:20 +0000

    mirco3 added

---
+++ micro3/src/main/java/ie/atu/micro3/Micro3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.micro3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Micro3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro3Application.class, args);
+	}
+
+}
