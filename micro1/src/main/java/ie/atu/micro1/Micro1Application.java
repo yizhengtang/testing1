bf35e2eaea561741a3d7318bd86a11f961569d7b
G00425075@atu.ie <g00425075@atu.ie>
2024-12-13 13:42:22 +0000

    edited micro1

--- micro1/src/main/java/ie/atu/micro1/Micro1Application.java
+++ micro1/src/main/java/ie/atu/micro1/Micro1Application.java
@@ -8,6 +8,7 @@ public class Micro1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Micro1Application.class, args);
+		SpringApplication.run(Micro1Application.class, args);
 	}
 
 }
